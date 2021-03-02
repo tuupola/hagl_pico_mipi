62c3a57e8ddcae4137bad7a8b5f62a354fcc032d
Mika Tuupola <tuupola@appelsiini.net>
2021-03-02 21:38:27 +0200

    Rename frame buffer to back buffer

--- hagl_hal_triple.c
+++ hagl_hal_triple.c
@@ -59,7 +59,7 @@ valid.
 static uint8_t buffer1[BITMAP_SIZE(DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_DEPTH)];
 static uint8_t buffer2[BITMAP_SIZE(DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_DEPTH)];
 
-static bitmap_t fb = {
+static bitmap_t bb = {
     .width = DISPLAY_WIDTH,
     .height = DISPLAY_HEIGHT,
     .depth = DISPLAY_DEPTH,
@@ -68,51 +68,51 @@ static bitmap_t fb = {
 bitmap_t *hagl_hal_init(void)
 {
     mipi_display_init();
-    bitmap_init(&fb, buffer2);
-    bitmap_init(&fb, buffer1);
+    bitmap_init(&bb, buffer2);
+    bitmap_init(&bb, buffer1);
 
-    hagl_hal_debug("Buffer 1 address is %p\n", (void *) buffer1);
-    hagl_hal_debug("Buffer 2 address is %p\n", (void *) buffer2);
+    hagl_hal_debug("Back buffer 1 address is %p\n", (void *) buffer1);
+    hagl_hal_debug("Back buffer 2 address is %p\n", (void *) buffer2);
 
-    return &fb;
+    return &bb;
 }
 
 void hagl_hal_flush()
 {
-    uint8_t *buffer = fb.buffer;
-    if (fb.buffer == buffer1) {
-        fb.buffer = buffer2;
+    uint8_t *buffer = bb.buffer;
+    if (bb.buffer == buffer1) {
+        bb.buffer = buffer2;
     } else {
-        fb.buffer = buffer1;
+        bb.buffer = buffer1;
     }
     /* Flush the current back buffer. */
-    mipi_display_write(0, 0, fb.width, fb.height, (uint8_t *) buffer);
+    mipi_display_write(0, 0, bb.width, bb.height, (uint8_t *) buffer);
 }
 
 void hagl_hal_put_pixel(int16_t x0, int16_t y0, color_t color)
 {
-    color_t *ptr = (color_t *) (fb.buffer + fb.pitch * y0 + (fb.depth / 8) * x0);
+    color_t *ptr = (color_t *) (bb.buffer + bb.pitch * y0 + (bb.depth / 8) * x0);
     *ptr = color;
 }
 
 color_t hagl_hal_get_pixel(int16_t x0, int16_t y0)
 {
-    return *(color_t *) (fb.buffer + fb.pitch * y0 + (fb.depth / 8) * x0);
+    return *(color_t *) (bb.buffer + bb.pitch * y0 + (bb.depth / 8) * x0);
 }
 
 void hagl_hal_blit(uint16_t x0, uint16_t y0, bitmap_t *src)
 {
-    bitmap_blit(x0, y0, src, &fb);
+    bitmap_blit(x0, y0, src, &bb);
 }
 
 void hagl_hal_scale_blit(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, bitmap_t *src)
 {
-    bitmap_scale_blit(x0, y0, w, h, src, &fb);
+    bitmap_scale_blit(x0, y0, w, h, src, &bb);
 }
 
 void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t width, color_t color)
 {
-    color_t *ptr = (color_t *) (fb.buffer + fb.pitch * y0 + (fb.depth / 8) * x0);
+    color_t *ptr = (color_t *) (bb.buffer + bb.pitch * y0 + (bb.depth / 8) * x0);
     for (uint16_t x = 0; x < width; x++) {
         *ptr++ = color;
     }
@@ -120,10 +120,10 @@ void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t width, color_t color)
 
 void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t height, color_t color)
 {
-    color_t *ptr = (color_t *) (fb.buffer + fb.pitch * y0 + (fb.depth / 8) * x0);
+    color_t *ptr = (color_t *) (bb.buffer + bb.pitch * y0 + (bb.depth / 8) * x0);
     for (uint16_t y = 0; y < height; y++) {
         *ptr = color;
-        ptr += fb.pitch / (fb.depth / 8);
+        ptr += bb.pitch / (bb.depth / 8);
     }
 }
 
