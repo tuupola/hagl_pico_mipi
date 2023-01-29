ba5ce78f13bdb61d1a3b22b73eae11b6c3e2f9ad
Mika Tuupola <tuupola@appelsiini.net>
2023-01-29 20:19:29 +0700

    Prefix internal defines with HAGL_PICO_MIPI_ (#28)

--- hagl_hal_single.c
+++ hagl_hal_single.c
@@ -66,7 +66,7 @@ blit(void *self, int16_t x0, int16_t y0, hagl_bitmap_t *src)
 static void
 hline(void *self, int16_t x0, int16_t y0, uint16_t width, color_t color)
 {
-    static color_t line[DISPLAY_WIDTH];
+    static color_t line[HAGL_PICO_MIPI_DISPLAY_WIDTH];
     const uint16_t height = 1;
 
     for (uint16_t x = 0; x < width; x++) {
@@ -80,7 +80,7 @@ hline(void *self, int16_t x0, int16_t y0, uint16_t width, color_t color)
 static void
 vline(void *self, int16_t x0, int16_t y0, uint16_t height, color_t color)
 {
-    static color_t line[DISPLAY_HEIGHT];
+    static color_t line[HAGL_PICO_MIPI_DISPLAY_HEIGHT];
     const uint16_t width = 1;
 
     for (uint16_t y = 0; y < height; y++) {
