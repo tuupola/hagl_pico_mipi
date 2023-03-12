d10dc45b476ca67a75a15f5c137167e22eb8c155
Mika Tuupola <tuupola@appelsiini.net>
2023-03-12 13:18:10 +0700

    Hello 2023!

--- include/mipi_dcs.h
+++ include/mipi_dcs.h
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2019-2022 Mika Tuupola
+Copyright (c) 2019-2023 Mika Tuupola
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
