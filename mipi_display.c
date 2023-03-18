a9c49120d23ac554f1f643198dd3cdf93c123b85
Mika Tuupola <tuupola@appelsiini.net>
2023-03-18 13:22:04 +0700

    Send only start coordinates when putting single pixel

--- mipi_display.c
+++ mipi_display.c
@@ -145,12 +145,18 @@ mipi_display_set_address(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
 {
     uint8_t command;
     uint8_t data[4];
+    size_t length = 4;
     static uint16_t prev_x1, prev_x2, prev_y1, prev_y2;
 
     x1 = x1 + MIPI_DISPLAY_OFFSET_X;
     y1 = y1 + MIPI_DISPLAY_OFFSET_Y;
     x2 = x2 + MIPI_DISPLAY_OFFSET_X;
     y2 = y2 + MIPI_DISPLAY_OFFSET_Y;
+
+    /* If putting only one pixel can ignore x2 and y2. */
+    if ((x1 == x2) && (y1 == y2)) {
+        length = 2;
+    }
 
     /* Change column address only if it has changed. */
     if ((prev_x1 != x1 || prev_x2 != x2)) {
@@ -159,7 +165,7 @@ mipi_display_set_address(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
         data[1] = x1 & 0xff;
         data[2] = x2 >> 8;
         data[3] = x2 & 0xff;
-        mipi_display_write_data(data, 4);
+        mipi_display_write_data(data, length);
 
         prev_x1 = x1;
         prev_x2 = x2;
@@ -172,7 +178,7 @@ mipi_display_set_address(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
         data[1] = y1 & 0xff;
         data[2] = y2 >> 8;
         data[3] = y2 & 0xff;
-        mipi_display_write_data(data, 4);
+        mipi_display_write_data(data, length);
 
         prev_y1 = y1;
         prev_y2 = y2;
